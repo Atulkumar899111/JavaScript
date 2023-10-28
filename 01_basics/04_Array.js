0f77b02202eeddfed3c813672a3f13aa354439b4
Atulkumar899111 <122602903+Atulkumar899111@users.noreply.github.com>
2023-10-28 15:14:48 +0000

    added the sum project

--- 01_basics/04_Array.js
+++ 01_basics/04_Array.js
@@ -1,3 +1,8 @@
-let arr1 = ["Test","Atul","Shruti"]
+let arr1 = ["Test","Atul","Shruti"];
 
-console.log(arr1[0]);
+// console.log(arr1[0]);
+arr1.push("Love");
+// console.log(arr1);
+// console.log(arr1.includes("Love"));
+
+console.log(`${arr1[1]} ${arr1[3]} ${arr1[2]}`);
---
+++ 01_basics/05_object.js
@@ -0,0 +1,9 @@
+let obj1 = {
+    name : "Atul",
+    Age : 26,
+    location : "Basti",
+    isLogedIn : true,
+    lastLogedIn : ["Monday",2]
+}
+console.log(obj1.Age);
+console.log(obj1.lastLogedIn)
